132772e63d8af83ccd67de713ff330df97b06831
yangyang <90810016+15982822539@users.noreply.github.com>
2024-06-03 16:33:08 +0800

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 style="color: #202122;">
+      <div rd-2 bg-transparent p-2 p-x-5 text-8 style="color: #202122;">
         ynzh003@163.com
       </div>
     </div>
