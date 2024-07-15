a2b01654a8a066bf7d4c75d580654c5ec5c23b9d
yangyang <90810016+15982822539@users.noreply.github.com>
2024-07-15 11:44:53 +0800

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-transparent p-2 p-x-5 text-4 color-black>
+      <div rd-2 bg-transparent p-2 p-x-5 text-4 style="color: #C4FD42;">
         new
       </div>
     </div>
