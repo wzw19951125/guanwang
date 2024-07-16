552cbbea9b37f63035da956e599a40d93c4fa2b7
yangyang <90810016+15982822539@users.noreply.github.com>
2024-07-16 14:50:57 +0800

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-transparent p-2 p-x-5 text-4 style="color: #C4FD42;">
-        new
+        wj100920230@163.com
       </div>
     </div>
   </div>
