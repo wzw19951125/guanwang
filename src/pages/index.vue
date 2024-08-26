234185d3f7b1fbb11a1d4f7b6a810bd61289a474
yangyang <90810016+15982822539@users.noreply.github.com>
2024-08-26 14:20:42 +0800

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-transparent p-2 p-x-5 text-5 style="color: #ffffff;">
-        leiguohao0811@163.com
+        new
       </div>
     </div>
   </div>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Nazzy'
+const appName = 'NEWNEW'
 </script>
 
 <template>
